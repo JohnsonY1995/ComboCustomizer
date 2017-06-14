38fd5cabceab1202b669790827d699ad6ffd5747
Johnson Yick <me@johnsonyick.com>
2017-06-15 00:02:35 +0800

    Enable HSQLDB server remote connection to view schema

--- src/main/java/fi/Application.java
+++ src/main/java/fi/Application.java
@@ -5,8 +5,10 @@ import org.hibernate.jpa.HibernateEntityManagerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(value="classpath:hsql_cfg.xml")
 public class Application {
 
     public static void main(String[] args) {
